02ad2def173352515bb1ba924a59d7c13f8ce2c1
Szymonexis <szymonkaszuba3@wp.pl>
2021-10-17 13:10:04 +0200

    Renewed the repo to its previous state - fixed pom configuration

---
+++ src/main/java/org/pickles/cvdesigner/MainWindow.java
@@ -0,0 +1,27 @@
+package org.pickles.cvdesigner;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import org.pickles.cvdesigner.enums.WindowSizes;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("mainWindow.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),
+                WindowSizes.MAIN_WINDOW_WIDTH.value,
+                WindowSizes.MAIN_WINDOW_HEIGHT.value);
+        stage.setTitle("CV-Designer");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/org/pickles/cvdesigner/enums/WindowSizes.java
@@ -0,0 +1,13 @@
+package org.pickles.cvdesigner.enums;
+
+public enum WindowSizes {
+    MAIN_WINDOW_WIDTH(320),
+    MAIN_WINDOW_HEIGHT(240);
+
+    public final Integer value;
+
+    WindowSizes(Integer value) {
+        this.value = value;
+    }
+}
+
