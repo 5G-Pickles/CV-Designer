7f01dd7a904c4b2445bee9780d4576abc492de4f
Karyna <karina.babenko16@gmail.com>
2021-10-18 16:39:06 +0200

    Create webview window

--- src/main/java/org/pickles/cvdesigner/enums/WindowSizes.java
+++ src/main/java/org/pickles/cvdesigner/enums/WindowSizes.java
@@ -2,7 +2,9 @@ package org.pickles.cvdesigner.enums;
 
 public enum WindowSizes {
     MAIN_WINDOW_WIDTH(320),
-    MAIN_WINDOW_HEIGHT(240);
+    MAIN_WINDOW_HEIGHT(240),
+    WEB_VIEW_WIDTH(1000),
+    WEB_VIEW_HEIGHT(1000);
 
     public final Integer value;
 
