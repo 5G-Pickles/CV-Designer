c4c5d31b468906a34591c09b2c0f87aeaf29df94
bkaryna <karyna.babenko@yahoo.com>
2021-11-26 03:03:25 +0100

    Set up basic hard skills validation

--- src/main/java/org/pickles/cvdesigner/controllers/HardSkillsController.java
+++ src/main/java/org/pickles/cvdesigner/controllers/HardSkillsController.java
@@ -1,14 +1,27 @@
 package org.pickles.cvdesigner.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.pickles.cvdesigner.enums.InputType;
 import org.pickles.cvdesigner.enums.ScenePaths;
 import org.pickles.cvdesigner.enums.SceneTitles;
+import org.pickles.cvdesigner.helpers.InvalidInputAlert;
+import org.pickles.cvdesigner.helpers.Styling;
+import org.pickles.cvdesigner.helpers.Validator;
 
 import java.io.IOException;
 
 public class HardSkillsController extends ControllerTemplate {
-    public void validateTopic(KeyEvent keyEvent) {
+    public TextField topicTextField;
+    public Label topicLabel;
+
+    public boolean validateTopic() {
+        String text = topicTextField.getText();
+        Styling.showError(topicLabel, Validator.textValid(text, false, true, InputType.CAPITALIZED));
+
+        return Validator.textValid(text, false, true, InputType.CAPITALIZED);
     }
 
     public void goBackToEmploymentHistory(ActionEvent actionEvent) throws IOException {
@@ -16,6 +29,12 @@ public class HardSkillsController extends ControllerTemplate {
     }
 
     public void goNextToSoftSkillsAndParse(ActionEvent actionEvent) throws IOException {
-        loadScene(SceneTitles.SOFT_SKILLS_TITLE.value, ScenePaths.SOFT_SKILLS_SCENE.value);
+        if (validateTopic()) {
+            loadScene(SceneTitles.SOFT_SKILLS_TITLE.value, ScenePaths.SOFT_SKILLS_SCENE.value);
+        } else {
+            Styling.showError(topicLabel, Validator.textValid(topicTextField.getText(), false, true, InputType.CAPITALIZED));
+            new InvalidInputAlert(Alert.AlertType.ERROR).showAndWait();
+        }
+
     }
 }
