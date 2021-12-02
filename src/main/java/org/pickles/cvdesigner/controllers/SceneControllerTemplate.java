a1308c32b06a8c479aab7e6bf668446f98cf942c
Szymonexis <szymonkaszuba3@wp.pl>
2021-12-02 23:51:24 +0100

    further develop controllers for data reading and writing from storage

--- src/main/java/org/pickles/cvdesigner/controllers/SceneControllerTemplate.java
+++ src/main/java/org/pickles/cvdesigner/controllers/SceneControllerTemplate.java
@@ -2,24 +2,28 @@ package org.pickles.cvdesigner.controllers;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.pickles.cvdesigner.Main;
 import org.pickles.cvdesigner.enums.SceneSizes;
 
 import java.io.IOException;
 
+import static org.pickles.cvdesigner.Main.fxmlLoader;
+import static org.pickles.cvdesigner.Main.scene;
+import static org.pickles.cvdesigner.Main.mainStage;
+
 public abstract class SceneControllerTemplate {
-    private static final Stage stage = Main.mainStage;
+    public static JSONObject onLoadData;
 
     static void loadScene(String sceneTitle, String resourceName) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(resourceName));
-        Scene scene = new Scene(fxmlLoader.load(),
+        fxmlLoader = new FXMLLoader(Main.class.getResource(resourceName));
+        scene = new Scene(fxmlLoader.load(),
                 SceneSizes.MAIN_WIDTH.value,
                 SceneSizes.MAIN_HEIGHT.value);
-        stage.setTitle(sceneTitle);
-        stage.setScene(scene);
-        stage.show();
+        mainStage.setTitle(sceneTitle);
+        mainStage.setScene(scene);
+        mainStage.show();
     }
 
     protected abstract boolean validateAll();
