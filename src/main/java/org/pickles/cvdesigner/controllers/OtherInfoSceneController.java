075c5afe1f32d51b35321527e8c9b932b67038dc
bkaryna <karyna.babenko@yahoo.com>
2021-12-05 13:20:21 +0100

    Implement write/load methods for other info

--- src/main/java/org/pickles/cvdesigner/controllers/OtherInfoSceneController.java
+++ src/main/java/org/pickles/cvdesigner/controllers/OtherInfoSceneController.java
@@ -5,28 +5,31 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import org.json.simple.parser.ParseException;
 import org.pickles.cvdesigner.alerts.InvalidInputErrorAlert;
+import org.pickles.cvdesigner.alerts.StorageNoDataInfoAlert;
+import org.pickles.cvdesigner.alerts.StorageWriteErrorAlert;
 import org.pickles.cvdesigner.enums.InputType;
 import org.pickles.cvdesigner.enums.ScenePaths;
 import org.pickles.cvdesigner.enums.SceneTitles;
 import org.pickles.cvdesigner.helpers.Styling;
 import org.pickles.cvdesigner.helpers.Validator;
+import org.pickles.cvdesigner.storage.OtherInfoSceneJsonStorage;
 
 import java.io.IOException;
 
 public class OtherInfoSceneController extends SceneControllerTemplate {
     public TextArea otherInfoTextArea;
-    public Label otherInfoLabel;
+    public Label sceneInfoLabel;
 
     public boolean validateOtherInfo() {
         String text = otherInfoTextArea.getText();
-        Styling.showError(otherInfoLabel, Validator.inputValid(text, false, true, InputType.CAPITALIZED));
-
+        Styling.showError(sceneInfoLabel, Validator.inputValid(text, false, true, InputType.CAPITALIZED));
         return Validator.inputValid(text, false, true, InputType.CAPITALIZED);
     }
 
     @Override
-    protected void loadData(ActionEvent actionEvent) {
-
+    protected void loadData(ActionEvent actionEvent) throws IOException, ParseException {
+        OtherInfoSceneJsonStorage sceneJsonStorage = new OtherInfoSceneJsonStorage();
+        fromStorageData = sceneJsonStorage.getDataFromStorage();
     }
 
     @Override
@@ -36,23 +39,39 @@ public class OtherInfoSceneController extends SceneControllerTemplate {
 
     @Override
     protected String writeDataToJson() throws IOException, ParseException {
-        return null;
+        OtherInfoSceneJsonStorage sceneJsonStorage = new OtherInfoSceneJsonStorage();
+        sceneJsonStorage.writePartialDataToSubJson(otherInfoTextArea.getId(), otherInfoTextArea.getText());
+        return sceneJsonStorage.writeToJsonStorage();
     }
 
     public void goBackToSoftSkillsScene(ActionEvent actionEvent) throws IOException {
         if (validateAll()) {
+            try {
+                writeDataToJson();
+            } catch (ParseException e) {
+                new StorageWriteErrorAlert();
+            }
             loadNextScene(SceneTitles.SOFT_SKILLS_SCENE_TITLE.value, ScenePaths.SOFT_SKILLS_SCENE.value);
         } else {
-            Styling.showError(otherInfoLabel, Validator.inputValid(otherInfoTextArea.getText(), false, true, InputType.CAPITALIZED));
+            Styling.showError(sceneInfoLabel, Validator.inputValid(otherInfoTextArea.getText(), false, true, InputType.CAPITALIZED));
             new InvalidInputErrorAlert().showAndWait();
+        }
+    }
+
+    public void goLoadDataOtherInfoScene(ActionEvent actionEvent) {
+        try {
+            this.loadData(actionEvent);
+            if (fromStorageData == null) {
+                new StorageNoDataInfoAlert();
+                return;
+            }
+            otherInfoTextArea.setText((String) fromStorageData.get(otherInfoTextArea.getId()));
+        } catch (IOException | ParseException e) {
+            new StorageNoDataInfoAlert();
         }
     }
 
     public void goNextToTemplatesSceneAndStoreData(ActionEvent actionEvent) throws IOException {
 
-    }
-
-    public void goLoadDataOtherInfoScene(ActionEvent actionEvent) {
-        this.loadData(actionEvent);
     }
 }
