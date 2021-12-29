eb37480650b8d32c61e6a6b39a68f8b7274703d7
Vyvr <lucianocoddingstuff@gmail.com>
2021-12-29 19:58:59 +0100

    update on grid-card interaction - analyze react-grid-layout-forked

--- designer/src/App.js
+++ designer/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Desinger from "./components/Desinger";
 import React from "react";
+import MyFirstGrid from "./components/Card";
 
 function App() {
   return (
-    <Desinger />
+    <MyFirstGrid />
   );
 }
 
--- designer/src/components/Desinger.js
+++ designer/src/components/Desinger.js
@@ -2,8 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import Paper from "./Paper";
-import RndCard from "./RndCard";
-import Card from "./RndCard";
+import MyFirstGrid from "./Card";
 
 const Content = styled.div`
   background-color: #004aad;
@@ -22,33 +21,30 @@ const Cards = styled.div`
 `;
 
 const Desinger = (props) => {
-  const sampleText =
-    "What is this?\n\nIt's a meme Batman...\nI don't understand...";
-
   return (
     <Content>
       <Cards>
-        <RndCard>
-          <h2>Header</h2>
-        </RndCard>
-        <RndCard>
-          <h2>Soft Skills</h2>
-        </RndCard>
-        <RndCard>
-          <h2>Hard Skills</h2>
-        </RndCard>
-        <RndCard>
-          <h2>Employment History</h2>
-        </RndCard>
-        <RndCard>
-          <h2>Education History</h2>
-        </RndCard>
-        <RndCard>
-          <h2>Other info</h2>
-        </RndCard>
-        <RndCard>
-          <h2>Footer</h2>
-        </RndCard>
+        {/*<RndCard>*/}
+        {/*  <h2>Header</h2>*/}
+        {/*</RndCard>*/}
+        {/*<RndCard>*/}
+        {/*  <h2>Soft Skills</h2>*/}
+        {/*</RndCard>*/}
+        {/*<RndCard>*/}
+        {/*  <h2>Hard Skills</h2>*/}
+        {/*</RndCard>*/}
+        {/*<RndCard>*/}
+        {/*  <h2>Employment History</h2>*/}
+        {/*</RndCard>*/}
+        {/*<RndCard>*/}
+        {/*  <h2>Education History</h2>*/}
+        {/*</RndCard>*/}
+        {/*<RndCard>*/}
+        {/*  <h2>Other info</h2>*/}
+        {/*</RndCard>*/}
+        {/*<RndCard>*/}
+        {/*  <h2>Footer</h2>*/}
+        {/*</RndCard>*/}
       </Cards>
       <Paper text="pepe" />
     </Content>
