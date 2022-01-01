5dfa355c84081b6a095e3713d2c95972747b1c1f
Szymonexis <szymonkaszuba3@wp.pl>
2022-01-01 23:11:32 +0100

    update the layout and future-proof data sending

--- designer/src/components/Desinger.js
+++ designer/src/components/Desinger.js
@@ -5,22 +5,45 @@ import Paper from "./Paper";
 
 const Content = styled.div`
   background-color: #004aad;
-  padding: 1rem;
-  maxHeight: 200px;
+  width: 1210px;
+  height: 1210px;
+  display:flex; 
+  flex-direction:row;
 `;
 
-const Cards = styled.div`
-  margin: 1px;
-  width: 25rem;
-  padding: 1rem;
+//742px should be the width
+const Container = styled.div`
+  height: 1210px;
+  width: 800px;
+  background-color: white;
+  border: grey 1px dotted;
+  border-radius: 16px;
+`;
+
+const Spacer = styled.div`
+  width: 205px;
   border: grey 1px dotted;
   border-radius: 16px;
 `;
 
 const Desinger = (props) => {
+    const texts = {
+        "footer": "footer",
+        "header": "header",
+        "soft": "soft",
+        "hard": "hard",
+        "info": "info",
+        "education": "education",
+        "job": "job",
+
+    }
   return (
     <Content>
-        <Paper />
+        <Spacer />
+        <Container>
+            <Paper texts={texts} />
+        </Container>
+        <Spacer />
     </Content>
   );
 };
--- designer/src/components/Paper.js
+++ designer/src/components/Paper.js
@@ -9,20 +9,23 @@ class Paper extends React.PureComponent {
     static defaultProps = {
         isDraggable: true,
         isResizable: true,
-        items: 5,
-        width: 800,
         rowHeight: 50,
         preventCollision: false,
-        cols: 24,
+        cols: 12,
         compactType: null,
     };
 
-    height = 2;
+    height = 17;
 
     state = {
         layout: [
-            { x: 0, y: 0, w: 1, h: 1, i: "test1" },
-            { x: 0, y: 0, w: 1, h: 1, i: "test2" }
+            { x: 0, y: 0, w: 12, h: 2, i: "header" },
+            { x: 0, y: 1, w: 3, h: 16, i: "info" },
+            { x: 3, y: 2, w: 9, h: 4, i: "education" },
+            { x: 3, y: 6, w: 9, h: 4, i: "job" },
+            { x: 3, y: 10, w: 9, h: 4, i: "hard" },
+            { x: 3, y: 14, w: 9, h: 4, i: "soft" },
+            { x: 0, y: 18, w: 12, h: 2, i: "footer", static: true }
         ]
     };
 
@@ -33,9 +36,13 @@ class Paper extends React.PureComponent {
                     {...this.props}
                     onLayoutChange={(layout) => {
                         layout.forEach((item) => {
-                            if (item.y > this.height) {
+                            if (item.y > this.height && item.i !== "footer") {
                                 item.y = this.height;
                             }
+                            if (item.h + item.y > this.height && item.i !== "footer") {
+                                item.h = this.height - item.y + 1
+                            }
+
                             console.log(layout)
                         })
                         this.setState({layout})
@@ -43,7 +50,7 @@ class Paper extends React.PureComponent {
                 >
                     {this.state.layout.map((item) => (
                         <div key={item.i} data-grid={item}>
-                            <span>{item.i}</span>
+                            <span>{this.props.texts[item.i]}</span>
                         </div>
                     ))}
                 </ReactGridLayout>
