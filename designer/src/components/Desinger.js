fe1f82b54f4ec416ae82c107285583c45a876a0d
Szymonexis <67708724+Szymonexis@users.noreply.github.com>
2022-01-03 20:08:22 +0100

    updates to designer.js

--- designer/src/components/Desinger.js
+++ designer/src/components/Desinger.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 import Paper from "./Paper";
-import data from "../storage.json";
 
 const Content = styled.div`
   background-color: #004aad;
@@ -30,41 +29,76 @@ const Spacer = styled.div`
 const getHeaderData = (data, prefix) => {
     const textFieldSuffix = "TextField";
     const sexRadioButtonToggleGroup = "sexRadioButtonToggleGroup";
-    const header = ["BasicData1SceneController"];
+    const header = prefix + "BasicData1SceneController";
+    const values = data[header];
 
     return {
-        "name": data[prefix + header[0]]["surname" + textFieldSuffix] + ", " + data[prefix + header[0]]["name" + textFieldSuffix],
-        "telephone": data[prefix + header[0]]["telephone" + textFieldSuffix],
-        "email": data[prefix + header[0]]["email" + textFieldSuffix],
+        title: values["surname" + textFieldSuffix] + ", " + values["name" + textFieldSuffix],
+        content: {
+            telephone: values["telephone" + textFieldSuffix],
+            email: values["email" + textFieldSuffix],
+        }
+    }
+}
+
+const getSoftData = (data, prefix) => {
+    const soft = prefix + "SoftSkillsSceneController";
+    const list = "softSkillsListView";
+    const values = data[soft][list];
+
+    return {
+        title: "Soft Skills",
+        content: values
+    }
+}
+
+const getHardData = (data, prefix) => {
+    const soft = prefix + "HardSkillsSceneController";
+    const list = "hardSkillsListView";
+    const values = data[soft][list];
+
+    let content = {};
+
+    for (const valuesKey in values) {
+        if (valuesKey !== "") {
+            console.log(values[valuesKey])
+        }
+    }
+
+    return {
+        title: "Hard Skills",
+        content: content,
     }
 }
 
 const getDataFromStorage = () => {
     const data = require("../storage.json");
-    console.log(data);
 
     const prefix = "org.pickles.cvdesigner.controllers.";
 
-    const soft = ["SoftSkillsSceneController"];
-    const hard = ["HardSkillsSceneController"];
     const education = ["EducationHistorySceneController"];
     const job = ["EmploymentHistorySceneController"];
     const info = ["BasicData1SceneController", "BasicData2SceneController",
         "OtherInfoSceneController", "HardSkillsSceneController"];
 
+    const footerStr = "I hereby give consent for my personal data included in the application to be" +
+        " processed for the purposes of the recruitment process in accordance with Art. 6 paragraph 1 " +
+        "letter a of the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 " +
+        "April 2016 on the protection of natural persons with regard to the processing of personal data " +
+        "and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation).";
 
-
-    const texts = {
-        footer: "I hereby give consent for my personal data included in the application to be processed for the purposes of the recruitment process in accordance with Art. 6 paragraph 1 letter a of the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation).",
+    return {
+        footer: {
+            title: footerStr,
+            content: footerStr,
+        },
         header: getHeaderData(data, prefix),
-        soft: "soft",
-        hard: "hard",
+        soft: getSoftData(data, prefix),
+        hard: getHardData(data, prefix),
         info: "info",
         education: "education",
         job: "job",
-    }
-
-    return texts;
+    };
 }
 
 const Desinger = (props) => {
