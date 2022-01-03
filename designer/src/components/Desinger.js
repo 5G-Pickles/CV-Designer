bea86cbe273d703a5c681fa2108200933e68af4a
Szymonexis <67708724+Szymonexis@users.noreply.github.com>
2022-01-03 16:38:06 +0100

    update to data storage access

--- designer/src/components/Desinger.js
+++ designer/src/components/Desinger.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import Paper from "./Paper";
+import data from "../storage.json";
 
 const Content = styled.div`
   background-color: #004aad;
@@ -26,20 +27,50 @@ const Spacer = styled.div`
   border-radius: 16px;
 `;
 
-const Desinger = (props) => {
+const getHeaderData = (data, prefix) => {
+    const textFieldSuffix = "TextField";
+    const sexRadioButtonToggleGroup = "sexRadioButtonToggleGroup";
+    const header = ["BasicData1SceneController"];
+
+    return {
+        "name": data[prefix + header[0]]["surname" + textFieldSuffix] + ", " + data[prefix + header[0]]["name" + textFieldSuffix],
+        "telephone": data[prefix + header[0]]["telephone" + textFieldSuffix],
+        "email": data[prefix + header[0]]["email" + textFieldSuffix],
+    }
+}
+
+const getDataFromStorage = () => {
     const data = require("../storage.json");
     console.log(data);
 
-    const texts = {
-        "footer": "footer",
-        "header": "header",
-        "soft": "soft",
-        "hard": "hard",
-        "info": "info",
-        "education": "education",
-        "job": "job",
+    const prefix = "org.pickles.cvdesigner.controllers.";
 
+    const soft = ["SoftSkillsSceneController"];
+    const hard = ["HardSkillsSceneController"];
+    const education = ["EducationHistorySceneController"];
+    const job = ["EmploymentHistorySceneController"];
+    const info = ["BasicData1SceneController", "BasicData2SceneController",
+        "OtherInfoSceneController", "HardSkillsSceneController"];
+
+
+
+    const texts = {
+        footer: "I hereby give consent for my personal data included in the application to be processed for the purposes of the recruitment process in accordance with Art. 6 paragraph 1 letter a of the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation).",
+        header: getHeaderData(data, prefix),
+        soft: "soft",
+        hard: "hard",
+        info: "info",
+        education: "education",
+        job: "job",
     }
+
+    return texts;
+}
+
+const Desinger = (props) => {
+    const texts = getDataFromStorage();
+    console.log(texts)
+
   return (
     <Content>
         <Spacer />
